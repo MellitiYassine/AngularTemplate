7303ac090e642fe134812794ed4c8b2d2afacae5
MellitiYassine <yassine.melliti@etudiant-fst.utm.tn>
2024-10-25 22:01:52 +0100

    feat: set menu bar and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenubarModule } from 'primeng/menubar';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SideBarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    FormsModule,
+    MenubarModule,
+    AvatarModule,
+    AvatarGroupModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
